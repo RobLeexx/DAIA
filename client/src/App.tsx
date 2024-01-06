6c8b5c4ee3742cbde99216cfb90d5983ef39824b
RobLeexx <roblee99.mc@gmail.com>
2024-01-05 23:37:42 -0400

    NavigationBarv1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SketchPage } from "./pages/SketchPage";
 import { SketchFormPage } from "./pages/SketchFormPage";
+import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route path="/sketches" element={<SketchPage />}></Route>
-        <Route path="/sketches" element={<SketchFormPage />}></Route>
+        <Route path="/sketches-create" element={<SketchFormPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
